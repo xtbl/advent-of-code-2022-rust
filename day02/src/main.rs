67e1c8ade7deb64d93b4cb37f8ac47d893a01b0b
Cristobal <correodecristobal@gmail.com>
2022-12-02 21:22:16 -0600

    day02 - part02

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -7,6 +7,22 @@ fn main() {
     println!("Day 2: Rock Paper Scissors");
 }
 
+fn get_game_score(play: (&str, &str)) -> i32 {
+    get_selected_shape_score(play) + get_outcome_score(play)
+}
+
+fn get_total_score(lines: Vec<String>) -> i32 {
+    let all_tuples: Vec<(&str, &str)> = lines.iter().map(|x| get_game_tuple(x)).collect();
+    all_tuples.iter().fold(0, |acc, x| acc + get_game_score(*x))
+}
+
+fn get_game_tuple(play: &str) -> (&str, &str) {
+    let mut split = play.split_whitespace();
+    // (split.next().unwrap(), convert_shape(split.next().unwrap()))
+    let opponent_shape = split.next().unwrap();
+    (opponent_shape, (get_shape_by_expected_outcome((opponent_shape, split.next().unwrap()))))
+}
+
 fn get_lines_from_file(filename: impl AsRef<Path>) -> Result<Vec<String>> {
     BufReader::new(File::open(filename)?).lines().collect()
 }
@@ -15,52 +31,25 @@ fn get_input_file_name() -> &'static str {
   "mock.txt"
 }
 
-fn get_game_tuple(play: &str) -> (&str, &str) {
-    let mut split = play.split_whitespace();
-    (split.next().unwrap(), convert_shape(split.next().unwrap()))
+// X lose, Y draw Z win
+fn get_shape_by_expected_outcome(play: (&str, &str)) -> &'static str {
+    match play {
+       ("A", "Y") => "A",
+       ("A", "X") => "C",
+       ("A", "Z") => "B",
+       ("B", "Y") => "B",
+       ("B", "X") => "A",
+       ("B", "Z") => "C",
+       ("C", "Y") => "C",
+       ("C", "X") => "B",
+       ("C", "Z") => "A",
+       (_, _) => "",
+    }
 }
 
-fn convert_shape(shape: &str) -> &str {
-    let converted_shape = match shape {
-        "X" => "A", // Rock
-        "Y" => "B", // Paper
-        "Z" => "C", // Scissors
-        _   => ""
-    };
-    converted_shape
-}
-
-
-//The first column is what your opponent is going to play: 
-// A for Rock, B for Paper, and C for Scissors.
-//The second column, you reason, must be what you should play in response: 
-// X for Rock, Y for Paper, and Z for Scissors
-
-// The score for a single round is the 
-//    score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors) 
-// plus the score for the outcome of the round 
 // (0 if you lost, 3 if the round was a draw, and 6 if you won).
-
-// how about converting second column to first column format each game?
-
-// check pattern matching https://doc.rust-lang.org/book/ch18-03-pattern-syntax.html
-
-
-
-fn get_selected_shape_score(play: (&str, &str)) -> i32 {
-//    score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
-    let score = match play {
-       (_, "A") => 1,
-       (_, "B") => 2,
-       (_, "C") => 3,
-        _  => 0,
-    };
-    score
-}
-
 fn get_outcome_score(play: (&str, &str)) -> i32 {
-// (0 if you lost, 3 if the round was a draw, and 6 if you won).
-    let score = match play {
+    match play {
        ("A", "A") => 3,
        ("B", "B") => 3,
        ("C", "C") => 3,
@@ -71,19 +60,28 @@ fn get_outcome_score(play: (&str, &str)) -> i32 {
        ("B", "A") => 0,
        ("A", "C") => 0,
        (_, _) => 0,
+    }
+}
+
+fn convert_shape(shape: &str) -> &str {
+    match shape {
+        "X" => "A", // Rock
+        "Y" => "B", // Paper
+        "Z" => "C", // Scissors
+        _   => ""
+    }
+}
+
+//    score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
+fn get_selected_shape_score(play: (&str, &str)) -> i32 {
+    let score = match play {
+       (_, "A") => 1,
+       (_, "B") => 2,
+       (_, "C") => 3,
+        _  => 0,
     };
     score
 }
-
-fn get_game_score(play: (&str, &str)) -> i32 {
-    let shape_score = get_selected_shape_score(play);
-    let outcome_score = get_outcome_score(play);
-    // println!("--- play: {:?}", play);
-    // println!("--- shape_score: {:?}", shape_score);
-    // println!("--- outcome_score: {:?}", outcome_score);
-    get_selected_shape_score(play) + get_outcome_score(play)
-}
-
 
 #[cfg(test)]
 mod tests {
@@ -96,15 +94,18 @@ mod tests {
             Ok(line) => line,
             Err(error) => panic!("Error getting line {:?}", error),
         };
-        let selected_line = &lines[0 as usize];
-        let game_tuple = get_game_tuple(selected_line);
-        let all_tuples: Vec<(&str, &str)> = lines.iter().map(|x| get_game_tuple(x)).collect();
-        // let all_scores: Vec<i32> = all_tuples.iter().map(|x| get_game_score(*x)).collect();
-        let all_scores: i32 = all_tuples.iter().fold(0, |acc, x| acc + get_game_score(*x));
-        println!("--- all_tuples: {:?}", all_tuples);
-        println!("--- all_scores: {:?}", all_scores);
+        let total_score = get_total_score(lines);
+        assert_eq!(total_score, 15);
+    }
 
-        assert_eq!(all_scores, 15);
+    #[test]
+    fn test_get_shape_by_expected_outcome() {
+        let game_01 = ("A", "Y");
+        let game_02 = ("B", "X");
+        let game_03 = ("C", "Z");
+        assert_eq!(get_shape_by_expected_outcome(game_01), "A");
+        assert_eq!(get_shape_by_expected_outcome(game_02), "A");
+        assert_eq!(get_shape_by_expected_outcome(game_03), "A");
     }
 
     #[test]
